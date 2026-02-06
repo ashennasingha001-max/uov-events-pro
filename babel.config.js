55a4c4fde9d18376c7c0b147094c2e6da7ee6251
Ashen Ranasingha <ashennasingha001@gmail.com>
2026-02-06 23:29:32 +0530

    Remove comment on plugin restrictions
    Removed comment about plugin restrictions in babel.config.js.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // මෙතන යටින් plugins: ['expo-router/babel'] වගේ ඒවා තියෙන්න බෑ!
+    
   };
 };
